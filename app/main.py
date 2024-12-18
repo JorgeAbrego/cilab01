a6681d3a7b162115cb2604c70e44133b091b332a
Jorge Abrego <jorge.abrego@gmail.com>
2024-12-18 00:59:50 +0000

    Feature: agregar endpoint about

--- app/main.py
+++ app/main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!!! :)"}
+
+
+@app.get("/about")
+async def about():
+    return {"version":"v1.0.0",
+            "author":"Jorge Abrego",
+            "message":"Jorge loves Agupon <3"
+            }
